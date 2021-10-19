38e87ed7b0728d302b78cae85cb1b44dc671ec1c
Matas <matas.vaitkus19@gmail.com>
2021-10-19 23:18:33 +0300

    Uzbaigta programa, viskas veikia

---
+++ event/FileLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @event
+{
+    //Klase skirta duomenu ivedimui i faila
+    class FileLogger
+    {
+        private string _path;
+        public FileLogger(string path) 
+        {
+            _path = path;
+        }
+
+        public void HandleWriteTransaction(string e)
+        {
+            
+            //string duomenys = $"Banko saskaite {bank.pavadinimas} pinigu kiekis pakito i {bank.likutis}";
+            if (File.Exists(_path))
+            {
+                using(StreamWriter sw = File.AppendText(_path))
+                {
+                    sw.WriteLine(e);
+                }
+            }
+            else
+            {
+                using(StreamWriter sw = File.CreateText(_path))
+                {
+                    sw.WriteLine(e);
+                }
+            }
+        }
+    }
+}
---
+++ event/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @event
+{
+    class Program
+    {
+        //public event EventHandler ManoEvent;
+
+        static void Main(string[] args)
+        {
+            string logPath = "../../Bank_logs.txt";
+            TikrintiLoga(logPath);      //istrina log.txt faila pries pradedant darba
+
+            //sukuriame banko sanskaitos objekta
+            Bankas manoBankas = new Bankas("Swed", 5268.62);
+
+
+            //sukuriame isvedimui skirtus objektus
+            FileLogger fl = new FileLogger(logPath);
+            EmailSender es = new EmailSender();
+
+            //eventam priskiriamos komandos
+            manoBankas.OnMoneyTransfered += (s, e) => fl.HandleWriteTransaction(e);
+            manoBankas.OnMoneyConversion += (s, e) => fl.HandleWriteTransaction(e);
+            manoBankas.OnMoneyAdded += (s, e) => fl.HandleWriteTransaction(e);
+            manoBankas.OnAnyTransaction += (s, e) => fl.HandleWriteTransaction(e);
+            
+            manoBankas.OnMoneyTransfered += (s, e) => es.HandlePrintTransaction(e);
+            manoBankas.OnMoneyConversion += (s, e) => es.HandlePrintTransaction(e);
+            manoBankas.OnMoneyAdded += (s, e) => es.HandlePrintTransaction(e);
+            manoBankas.OnAnyTransaction += (s, e) => es.HandlePrintTransaction(e);
+            
+            ;
+
+            //vykdomi veiksmai
+            manoBankas.IdetiPinigus(300);
+            Console.WriteLine();
+
+            manoBankas.PervestiPinigus(256);
+            Console.WriteLine();
+
+            manoBankas.KeistiPinigus(0.6);
+
+            Console.ReadKey();
+        }
+
+        static void TikrintiLoga(string path)   //jei randa pasilikusi sena log, ji istrina
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
